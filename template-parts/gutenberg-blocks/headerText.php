ebf67531c2caadd2a307eccb21d61aa8f60081ee
BennyXizt <64214352+BennyXizt@users.noreply.github.com>
2025-12-19 02:49:36 +0100

    [YPDATE]: Updated theme

--- template-parts/gutenberg-blocks/headerText.php
+++ template-parts/gutenberg-blocks/headerText.php
@@ -1,34 +1,33 @@
 <?php
-
-    $allowed_title_tags = [
+   $allowed_title_tags = [
         'span' => []
     ];
     $fields = [
-        'title' => wp_kses(get_field('title'), $allowed_title_tags),
         'tag' => get_field('tag'),
-        'type' => get_field('type'),
+        'title' => wp_kses(get_field('title'), $allowed_title_tags),
+        'type' => get_field('title_type') ?? 'h2',
+        'title_color' => get_field('title_color'),
         'text' => get_field('text'),
     ];
 
-    $has_content = array_filter($fields);
+    $title_color = $fields['title_color'] ? 'style="color: '. $fields['title_color'] .'"' : '';
+
 ?>
 
-<article class="headerText">
+ <article class="headerText">
     <?php if($fields['tag']) : ?>
         <div class="headerText__tag">
             <?= esc_html($fields['tag']) ?>
         </div>
     <?php endif; ?>
     <?php if($fields['title']) : ?>
-        <<?=$fields['type']?> class="headerText__title">
+        <<?=$fields['type']?> class="headerText__title" <?=$title_color ?>>
             <?= $fields['title'] ?>
         </<?=$fields['type']?>>
     <?php endif; ?>
     <?php if($fields['text']) : ?>
         <div class="text">
-            <?= $fields['text']?>
+            <?= wp_kses_post($fields['text']) ?>
         </div>
     <?php endif; ?>
-
-    <InnerBlocks />
 </article>
