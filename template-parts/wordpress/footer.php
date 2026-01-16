bbd5b16a343270993e552278bff804010112dc31
BennyXizt <zymayvetaliy@gmail.com>
2026-01-16 22:26:54 +0100

    [UPDATE]: Added footer

--- template-parts/wordpress/footer.php
+++ template-parts/wordpress/footer.php
@@ -1,5 +1,122 @@
+<?php 
+    $locations = get_nav_menu_locations();
+    $socialsID = $locations['socials'];
+    $footerID = $locations['footer_menu'];
+    $socials = wp_get_nav_menu_items($socialsID);
+    $footerMenu = util_buildMenu(wp_get_nav_menu_items($footerID));
+
+    function generateFooterMenu($menu, $class = 'body-footer', $dept = 0) {
+        if(empty( $menu )) return;
+
+        echo '<ul class="body-footer__list">';
+        foreach($menu as $element) {
+            echo "<li class='{$class}__item". ($element['children'] ? " {$class}__item--submenu submenu-{$dept}" : "") ."'>";
+                echo '<article data-fsc-accordion data-fsc-accordion-behaviour="default" data-fsc-accordion-media-query="max-width: 767.98px" class="body-footer__accordion accordion">';
+                    echo '<div data-fsc-accordion-summary class="accordion__top">';
+                        echo '<h6 class="accordion__title">'. $element['item']->title .'</h6>';
+                        echo '<svg class="accordion__icon">';
+                            echo '<use href="'. get_template_directory_uri() .'/assets/media/icons/sprite.svg#ph_plus-light"></use>';
+                        echo '</svg>';
+                    echo '</div>';
+                    echo '<ul data-fsc-accordion-body class="accordion__body">';
+                    if($element['children']) {
+                        foreach ($element['children'] as $child) {
+                            echo '<li>'. $child['item']->title .'</li>';
+                        }
+                    }
+                    echo '</ul>';
+                echo '</article>';
+            echo '</li>';
+        }
+        echo '</ul>';
+    }
+?>
+
 <footer class="footer">
-    <div class="footer__container _container">
-    
+    <div class="footer__container container">
+        <div class="footer__body body-footer">
+            <div class="body-footer__content content-footer">
+                <?php util_displayLogo('content-footer'); ?>
+                <?php if(!empty($socials)) : ?>
+                    <ul class="content-footer__social social social--vertical">
+                        <?php foreach($socials as $social) : ?>
+                            <li class="social__item">
+                                <?php if(!empty($social->url)) : ?>
+                                    <a href="<?= esc_url($social->url) ?>" class="social__link">
+                                <?php endif; ?>
+                                <?= util_getIcon($social->url); ?>
+                                <?= $social->title ?>
+                                <?php if(!empty($social->url)) : ?>
+                                    </a>
+                                <?php endif; ?>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endif; ?>
+            </div>
+            <?php if(!empty($footerMenu )) : ?>
+                <?php generateFooterMenu($footerMenu); ?>
+                <!-- <ul class="body-footer__list">
+                    <?php foreach($footerMenu as $footer) : ?>
+                        <li class="body-footer__item">
+                            <article
+                                data-fsc-accordion
+                                data-fsc-accordion-behaviour="default"
+                                data-fsc-accordion-media-query="max-width: 767.98px"
+                                class="body-footer__accordion accordion"
+                            >
+                                <div data-fsc-accordion-summary class="accordion__top">
+                                    <h6 class="accordion__title">Pages</h6>
+
+                                    <svg class="accordion__icon">
+                                        <use href="./assets/media/sprite.svg#ph_plus-light"></use>
+                                    </svg>
+                                </div>
+                                <ul data-fsc-accordion-body class="accordion__body">
+                                    <li>Home</li>
+
+                                    <li>Services</li>
+
+                                    <li>About</li>
+
+                                    <li>Contact</li>
+                                </ul>
+                            </article>
+
+                            <button class="button button--size-small button--style-primary">
+                                More Templates
+
+                                <svg class="icon">
+                                <use
+                                    href="./media/icons/sprite.svg#ph_arrow-up-right-light"
+                                ></use>
+                                </svg>
+                            </button>
+                        </li>
+                    <?php endforeach; ?>
+                </ul> -->
+            <?php endif; ?>
+        </div>
+        <div class="footer__bottom bottom-footer">
+            <div class="bottom-footer__copyright">
+                © 2022 Made by Pawel Gola. Powered by Framer..
+            </div>
+
+            <button
+                data-fsc-scroll
+                data-fsc-scroll-to="main"
+                data-fsc-scroll-behaviour="smooth"
+                data-fsc-scroll-block="start"
+                class="bottom-footer__up button button--type-button-text"
+            >
+                To Top
+
+                <div class="bottom-footer__icon-rounded icon-rounded">
+                <svg class="icon-rounded__icon">
+                    <use href="./media/icons/sprite.svg#ph_arrow-up-light"></use>
+                </svg>
+                </div>
+            </button>
+        </div>
     </div>
 </footer>
--- template-parts/wordpress/header.php
+++ template-parts/wordpress/header.php
@@ -1,13 +1,4 @@
 <?php
-    $custom_logo_id = get_theme_mod('custom_logo'); 
-    $logo_file = get_attached_file($custom_logo_id);
-
-    if($logo_file) {
-        $logo_ext = pathinfo($logo_file, PATHINFO_EXTENSION);
-        $logo_svg = file_get_contents($logo_file);
-        $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
-    }
-
     $locations = get_nav_menu_locations();
     $desktopMenuID = $locations['header_menu'];
     $desktopMenyButtonsID = $locations['header_buttons'];
@@ -18,23 +9,7 @@
 <header class="header">
     <div class="header__container container">
         <div class="header__body">
-             <?php if(is_front_page() || is_home()) : ?>
-                <figure class="header__logo">
-                    <?php if($logo_file && $logo_ext === 'svg') :?>
-                        <?= $logo_svg;?>
-                    <?php elseif($logo_file && $logo_ext !== 'svg') : ?>
-                        <img src="<?= esc_url($logo[0]); ?>" width="<?= $logo[1]?>" height="<?= $logo[2]?>" alt="<?= bloginfo('name'); ?>_logo">
-                    <?php endif; ?>
-                </figure>
-            <?php else : ?>
-                <a href="<?= esc_url(home_url('/'));?>" class="header__logo">
-                    <?php if($logo_file && $logo_ext === 'svg') :?>
-                        <?= $logo_svg;?>
-                    <?php elseif($logo_file && $logo_ext !== 'svg') : ?>
-                        <img src="<?= esc_url($logo[0]); ?>" width="<?= $logo[1]?>" height="<?= $logo[2]?>" alt="<?= bloginfo('name'); ?>_logo">
-                    <?php endif; ?>
-                </a>
-            <?php endif; ?>
+            <?php util_displayLogo('header') ?>
             <div class="header__menu menu">
                 <nav class="menu__body">
                     <?php util_generateMenus($desktopMenu)?>
